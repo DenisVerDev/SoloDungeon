6e9161dcafbb11d3aae19486deb85e6c0d3756a0
Denis <me@gg.com>
2022-05-04 21:14:52 +0300

    added settings class and diftools

---
+++ src/headers/Settings.h
@@ -0,0 +1,91 @@
+#pragma once
+#include<string>
+#include<fstream>
+#include"DifferentTools.h"
+#include<SFML/Window.hpp>
+
+/*-------------------------------------------------------------------------------------------
+	All settings can be divided in two categories:
+	1) settings that can be changed during the gameplay					->	changeable
+	2) settings that are initialized from the start and not changeable	->	non changeable
+
+	Settings class:
+	- store all necessary infornation about rendering process and audio settings
+	- private constructor => only static functions and variables
+	- two different states of render: frame dependent and independent
+	- setter for 'iconfile_name' checks if such file exist, if negative => NONE
+	- save settings in file
+	- load settings from file
+	[TODO]:1
+	- applies render and audio settings in case they were changed
+	[TODO]:2
+	- try catch blocks, add custom exception class and log class to keep track of errors
+	[TODO]:3
+	- icons_path should be in some Resource class or something
+-------------------------------------------------------------------------------------------*/
+
+
+enum class FrameDependency
+{
+	Dependent,
+	Free
+};
+
+class Settings
+{
+private:
+
+	Settings();		//private constructor in order to avoid making an object of this class
+
+	//------PRIVATE VARS------
+	static unsigned int frame_limit; 
+
+	static float music_volume;	
+	static float sound_volume;	
+
+	static std::string iconfile_name;			// non changeable
+	static std::string window_title;			// non changeable
+
+public:
+
+	//------CONSTANTS------
+	static const unsigned int max_frame_limit;	// non changeable
+	static const unsigned int min_frame_limit;	// non changeable
+
+	static const std::string icons_path;			// non changeable
+
+	static const sf::VideoMode video_mode;		// non changeable | only one video mode
+	
+	//------PUBLIC VARS------
+	static bool vertical_sync;
+
+	static FrameDependency dependency_mode;
+
+	//------METHODS------
+	static void applySettings(); //[TODO]:1
+
+	static void saveSettings(); 
+
+	static void loadSettings(); 
+
+	static void setStandartSettings();
+
+	//------SETTERS------
+	static void setFrameLimit(unsigned int frame_limit);
+	static void setMusicVolume(float volume);
+	static void setSoundVolume(float volume);
+	static void setIconFileName(std::string filename);
+	static void setWindowTitle(std::string title);
+	static void setDependencyMode(unsigned int dependecy_mode);
+
+	//------GETTERS------
+	static int getFrameLimit();
+	static float getMusicVolume();
+	static float getSoundVolume();
+	static std::string getIconFileName();
+	static std::string getWindowTitle();
+
+};
+
+
+std::ifstream& operator>>(std::ifstream& stream, FrameDependency& fd); //overloading ifstream operator >> to read [int] and convert it to [FrameDependency]
---
+++ src/settings.cpp
@@ -0,0 +1,208 @@
+#include"headers/Settings.h"
+
+//------Initializing constants------
+const unsigned int Settings::max_frame_limit = 144;
+const unsigned int Settings::min_frame_limit = 30;
+const std::string Settings::icons_path = "res/icons/";
+const sf::VideoMode Settings::video_mode = sf::VideoMode::getFullscreenModes()[0]; //gets the highest resolution possible
+
+//------Initializing variables------
+unsigned int Settings::frame_limit = 60;
+
+float Settings::music_volume = 100.f;
+float Settings::sound_volume = 100.f;
+
+bool Settings::vertical_sync = false;
+
+std::string Settings::window_title = "Standart game window";
+std::string Settings::iconfile_name = "NONE";
+
+FrameDependency Settings::dependency_mode = FrameDependency::Dependent;
+
+//------Methods definition------
+void Settings::applySettings() //[TODO]:1
+{
+
+}
+
+void Settings::saveSettings() //save settings to file
+{
+	std::string path = "configs/settings.conf";
+	std::ofstream ostream;
+
+	try
+	{
+		ostream.open(path);
+
+		if (ostream.good())
+		{
+			ostream << "frame_limit=" << Settings::frame_limit << "\n";
+			ostream << "vertical_sync=" << Settings::vertical_sync << "\n";
+			ostream << "music_volume=" << Settings::music_volume << "\n";
+			ostream << "sound_volume=" << Settings::sound_volume << "\n";
+			ostream << "iconfile_name=" << Settings::iconfile_name << "\n";
+			ostream << "window_title=" << Settings::window_title << "\n";
+			ostream << "dependency_mode=" << (int)Settings::dependency_mode << "\n";
+		}
+
+		ostream.close();
+	}
+	catch (...) //[TODO]:2
+	{
+		ostream.close();
+	}
+}
+
+void Settings::loadSettings() //load settings from file
+{
+	std::string path = "configs/settings.conf";
+	std::ifstream istream;
+
+	try
+	{
+		istream.open(path);
+
+		if (istream.good())
+		{
+			std::string buff;
+
+			std::getline(istream, buff, '=');
+			unsigned int fl;
+			istream >> fl;
+			Settings::setFrameLimit(fl);
+			rEnd(istream, buff)
+
+			std::getline(istream, buff, '=');
+			istream >> Settings::vertical_sync;
+			rEnd(istream, buff)
+
+			std::getline(istream, buff, '=');
+			float mv;
+			istream >> mv;
+			Settings::setMusicVolume(mv);
+			rEnd(istream, buff)
+
+			std::getline(istream, buff, '=');
+			float sv;
+			istream >> sv;
+			Settings::setSoundVolume(sv);
+			rEnd(istream, buff)
+
+			std::getline(istream, buff, '=');
+			std::string iconfn;
+			istream >> iconfn;
+			Settings::setIconFileName(iconfn);
+			rEnd(istream, buff)
+
+			std::getline(istream, buff, '=');
+			std::string wt;
+			std::getline(istream, wt);
+			Settings::setWindowTitle(wt);
+			//- dont require rEnd there because [>>] stops at first space
+
+			std::getline(istream, buff, '=');
+			istream >> Settings::dependency_mode;
+			rEnd(istream, buff)
+
+		}
+
+		istream.close();
+	}
+	catch (...) //[TODO]:2
+	{
+		istream.close();
+		Settings::setStandartSettings();
+	}
+}
+
+//------Setters definition------
+void Settings::setStandartSettings()
+{
+	Settings::setFrameLimit(60);
+	Settings::vertical_sync = false;
+	Settings::dependency_mode = FrameDependency::Dependent;
+	Settings::setMusicVolume(100.f);
+	Settings::setSoundVolume(100.f);
+	Settings::setWindowTitle("Standart game window");
+	Settings::setIconFileName("NONE");
+}
+
+void Settings::setFrameLimit(unsigned int frame_limit)
+{
+	if (frame_limit >= Settings::min_frame_limit && frame_limit <= Settings::max_frame_limit) Settings::frame_limit = frame_limit;
+	else Settings::frame_limit = 60;
+}
+
+void Settings::setMusicVolume(float volume)
+{
+	if (volume >= 0.f && volume <= 100.f) Settings::music_volume = volume;
+	else Settings::music_volume = 100.f;
+}
+
+void Settings::setSoundVolume(float volume)
+{
+	if (volume >= 0.f && volume <= 100.f) Settings::sound_volume = volume;
+	else Settings::sound_volume = 100.f;
+}
+
+void Settings::setIconFileName(std::string filename)
+{
+	std::string fullpath = Settings::icons_path + filename;
+	try
+	{
+		struct stat buffer;
+		if (stat(fullpath.c_str(), &buffer) == 0) Settings::iconfile_name = filename;
+		else Settings::iconfile_name = "NONE";
+	}
+	catch (...) //[TODO]:2
+	{
+		Settings::iconfile_name = "NONE";
+	}
+}
+
+void Settings::setWindowTitle(std::string title)
+{
+	if (!title.empty()) Settings::window_title = title;
+	else Settings::window_title = "Standart game window";
+}
+
+void Settings::setDependencyMode(unsigned int dependecy_mode)
+{
+	if (dependecy_mode == 0 || dependecy_mode == 1) Settings::dependency_mode = (FrameDependency)dependecy_mode;
+	else Settings::dependency_mode = FrameDependency::Dependent;
+}
+
+//------Getters definition------
+int Settings::getFrameLimit()
+{
+	return Settings::frame_limit;
+}
+
+float Settings::getMusicVolume()
+{
+	return Settings::music_volume;
+}
+
+float Settings::getSoundVolume()
+{
+	return Settings::sound_volume;
+}
+
+std::string Settings::getIconFileName()
+{
+	return Settings::iconfile_name;
+}
+
+std::string Settings::getWindowTitle()
+{
+	return Settings::window_title;
+}
+
+//------Overload operators definition------
+std::ifstream& operator>>(std::ifstream& stream, FrameDependency& fd)
+{
+	int idm; //int dependecy_mode
+	stream >> idm;
+	Settings::setDependencyMode(idm);
+	return stream;
+}
