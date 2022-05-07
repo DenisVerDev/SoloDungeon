bcf125687cbd1f2a4ee4303fbb4e9af6f865857f
Denis <me@gg.com>
2022-05-07 17:31:33 +0300

    added empty GameScene class

---
+++ src/headers/GameScene.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/*-------------------------------------------------------------------------------------------
+	GameScene class:
+	- stores every game object
+	- updates logic and behaviour of the game
+	- works in different thread
+-------------------------------------------------------------------------------------------*/
+
+class GameScene
+{
+	// empty class just for now
+};
